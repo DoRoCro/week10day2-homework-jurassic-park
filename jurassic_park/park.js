b0d87b8f980ff3e90dc12af7d7b7e5de6024d7e5
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-04 19:45:03 +0100

    define test for adding a dinosaur to park

--- jurassic_park/park.js
+++ jurassic_park/park.js
@@ -1,7 +1,12 @@
 var Park = function(name) {
   this.name = name;
   this.numberDinosaurs = 0;
-
+  this.dinos = [];
 }
+
+Park.prototype.addDino = function(dinosaur) {
+  this.dinos.push(dinosaur);
+  this.numberDinosaurs = this.dinos.length;
+};
 
 module.exports = Park;
--- jurassic_park/specs/park_spec.js
+++ jurassic_park/specs/park_spec.js
@@ -1,12 +1,15 @@
 var assert = require('assert');
 var Park = require('../park.js');
+var Dinosaur = require('../dinosaur.js');
 
 describe('Park', function() {
 
   var park1;
+  var dino1
 
   beforeEach('before each', function() {
     park1 = new Park('We\'re all going to die',1);
+    dino1 = new Dinosaur('Stegosaurus', 2)
   })
 
   it('should have a name', function(){
@@ -15,5 +18,10 @@ describe('Park', function() {
 
   it('Should start with no dinosaurs', function() {
     assert.strictEqual(0, park1.numberDinosaurs);
+  })
+
+  it('can add a dinosaur', function(){
+    park1.addDino(dino1);
+    assert.strictEqual(1, park1.numberDinosaurs)
   })
 })
